3d7b1c313b4ddda2efb69886457fe5a857209b62
katomasato <hankun0127taktak@gmail.com>
2019-09-09 22:54:43 +0900

    課題だお
    課題０１

---
+++ kato/helloworld/pages/hello.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="color">
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return { message: 'HelloWorld' }
+  }
+}
+
+
+
+</script>
+
+<style>
+.color{
+    font-size: 100px;
+    color: red;
+    font-weight: bold;
+    letter-spacing: 50px;
+}
+</style>
