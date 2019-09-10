fd7a774232654847132127c80434ece51d87f94e
mitsuru170 <mitsuru326323@icloud.com>
2019-09-10 15:00:29 +0900

    提出期限守れなくてすいません

---
+++ mabuchi/HelloWorld/.nuxt/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _1fdb70fa = () => interopDefault(import('..\\pages\\inspire.vue' /* webpackChunkName: "pages_inspire" */))
+const _31b6ecf2 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+      path: "/inspire",
+      component: _1fdb70fa,
+      name: "inspire"
+    }, {
+      path: "/",
+      component: _31b6ecf2,
+      name: "index"
+    }],
+
+  fallback: false
+}
+
+export function createRouter() {
+  return new Router(routerOptions)
+}
