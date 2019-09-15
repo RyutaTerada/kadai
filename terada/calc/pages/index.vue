ac5a6d503989783f59886be5a465cfbbba6ea330
Ryuta <revos347@gmail.com>
2019-09-15 10:30:59 +0900

    枠の完成＆エラーで困ってる
    ReferenceError:  is not defined
    これどうすんの？？

--- terada/calc/pages/index.vue
+++ terada/calc/pages/index.vue
@@ -1,92 +1,79 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+    <div>
+        <div>
+            <!-- 計算結果を表示 -->
+            <h1>{{ myResult }}</h1><hr>
+        </div>
+        <div>
+            <!-- 各種ボタン -->
+            <!-- v-btnタグでvuetifyのボタン呼び出し -->
+            <!-- v-on:click.[native]いるかよくわからん -->
+            <v-btn v-on:click="runC()">C</v-btn>
+            <v-btn v-on:click="runCalc('/')">/</v-btn>
+            <v-btn v-on:click.native="addValue(7)">7</v-btn>
+            <v-btn v-on:click="addValue(8)">8</v-btn>
+            <v-btn v-on:click="addValue(9)">9</v-btn>
+            <v-btn v-on:click="runCalc('*')">*</v-btn>
+            <v-btn v-on:click="addValue(4)">4</v-btn>
+            <v-btn v-on:click="addValue(5)">5</v-btn>
+            <v-btn v-on:click="addValue(6)">6</v-btn>
+            <v-btn v-on:click="runCalc('-')">-</v-btn>
+            <v-btn v-on:click="addValue(1)">1</v-btn>
+            <v-btn v-on:click="addValue(2)">2</v-btn>
+            <v-btn v-on:click="addValue(3)">3</v-btn>
+            <v-btn v-on:click="runCalc('+')">+</v-btn>
+            <v-btn v-on:click="addValue(0)">0</v-btn>
+            <v-btn v-on:click="addValue('.')">.</v-btn>
+            <v-btn v-on:click="runCalc('=')">=</v-btn>
+        </div>
+    </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+export default{
+    // FIXME:ReferenceError: myFlg is not defined
+    // FIXME:意味わからん
+    data(){  //変数宣言するところ
+        return{
+           myTotal : 0,  //現在の合計値
+           myInput : '',  //現在入力している値
+           myCalc : '+',  //合計と入力値の演算子
+           myFlg : 1,  //1回前に入力したもの 0:数字 1:演算子
+           myResult : 0,
+        }
+    },
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+    methods: {  //関数処理をするところ
+        addValue: function(myData){  //数字or小数点の入力
+            myFlg = 0;  //前回は数字の入力
+            myInput += myData;  //入力している値に追加
+            myResult = myInput;  //入力している値を表示
+        },
+        runCalc(myData){  //演算子を押した
+            if (myFlg == 0) {  //前回の入力は数字か？
+                myFlg = 1;  //前回の入力は演算子
+                myWork = myTotal + myCalc + myInput;  //計算式の作成
+                myTotal = eval(myWork);  //計算の実行
+                myInput = ''; //入力した値のクリア
+                myResult = myInput; //合計出力
+            }
+            if(myData == '='){ //＝を押したか？
+                myTotal = 0;  //合計クリア
+                myCalc = '+';  //演算子を+に 
+            }else{
+                myCalc = myData; //演算子の退避
+            }
+        },
+        runC(){ //クリアボタンを押した
+            myTotal = 0; //合計クリア
+            myCalc = '+'; //演算子のクリア
+            myInput = '';  //現在入力している値をクリア
+            myResult = myTotal; //表示を0に
+        }
+    },
 }
 </script>
+
+<style>
+/* TODO:スタイルいじりは後回し */
+</style>
