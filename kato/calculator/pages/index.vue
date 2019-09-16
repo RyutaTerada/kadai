ff45c3f89c51180c1e825f001a9b4f9c4b7f9e12
katomasato <hankun0127taktak@gmail.com>
2019-09-16 22:38:46 +0900

    書き方は多分あってるけどエラー
    ボタンが動かない

--- kato/calculator/pages/index.vue
+++ kato/calculator/pages/index.vue
@@ -50,12 +50,33 @@
         myFlg : 1,	// １回前に入力したもの 0:数字 1:演算子
       };
     },
-    methods :{
-      function  : myValue(myData){	// 0～9または小数点ボタンを押した
+    methods : {
+      hyouji : function myValue(myData){	// 0～9または小数点ボタンを押した
         myFlg = 0;	// １回前に入力したものは数値
         myInput += myData;	// 現在入力している値に追加
         document.myForm.myLine.value = myInput;	// 現在入力している値表示
-      };
+      },
+      ennzan : function myCalculate(myData){	// 演算ボタンを押した
+        if (myFlg==0){	// １回前に入力したものは数値か？
+          myFlg = 1;	// １回前に入力したものは演算子
+          myWork = myTotal + myCalc + myInput;	// 一連の計算式を作る
+          myTotal = eval(myWork);	// 計算式を計算させる
+          myInput = "";	// 現在入力している値をクリア
+          document.myForm.myLine.value = myTotal;	// 合計を表示
+        }	
+        if (myData == "="){	// 演算ボタンは[＝]か？
+          myTotal = 0;	// 合計をクリア
+          myCalc = "+";	// 演算子を[+]とする
+          }else{	// 演算ボタンは[＝]以外である
+          myCalc = myData;	// 演算子を退避させておく
+        }	;
+      },
+      clear : function myC(){	// クリアボタン[C]を押した
+        myTotal = 0;	// 合計クリア
+        myCalc = "+";	// 演算子クリア
+        myInput = "";	// 現在入力している値をクリア
+        document.myForm.myLine.value = myTotal;	// つまり、０を表示
+      }
     }
   };
 </script>
